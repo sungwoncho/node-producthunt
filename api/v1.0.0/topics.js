bd2049914ab246f9d0fce8ab95712df88a704445
clemDelp <delpuech.clement@gmail.com>
2017-08-01 00:16:16 +0200

    update comment for doc

--- api/v1.0.0/topics.js
+++ api/v1.0.0/topics.js
@@ -4,11 +4,11 @@ var topics = {};
 
 module.exports = function (client) {
   /**
-   * @summary List of all categories
-   * @memberof categories
+   * @summary List of all topics
+   * @memberof topics
    * @param {done} done - Callback
    *
-   * @see https://api.producthunt.com/v1/docs/categories/categories_index_list_of_all_categories
+   * @see https://api.producthunt.com/v1/docs/topics/topics_index_list_topics
    */
   topics.index = function (done) {
     client.httpGet('/topics', {}, done);
